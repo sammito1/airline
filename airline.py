4169818ddb5280bbfb9e0b4a80a0bc29ce06b1f1
sammito1 <sam.c.melendez2@gmail.com>
2020-11-29 03:19:17 -0600

    working on frontend

--- airline.py
+++ airline.py
@@ -8,6 +8,9 @@ def index():
     """ Display available flights """ 
     db = SQL("postgres://postgres:postgres@localhost:5432/COSC3380") # local db
     flights_query = db.execute("SELECT * FROM app_flights")
-    
+
     # db = SQL("postgres://cosc0168:1934844MS@code.cs.uh.edu:5432/COSC3380") # remote db
+
+    # format data in returned rows
+
     return render_template('index.html', rows=flights_query)
