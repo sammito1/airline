8b203cf53b991bf9a11f309b6519059520a4e491
sammito1 <sam.c.melendez2@gmail.com>
2020-12-18 12:44:44 -0600

    Refactored code, shifted to signed cookie sessions for simplicity

--- application.py
+++ application.py
@@ -2,7 +2,6 @@ import datetime, os, helpers
 from helpers import *
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session, url_for
-from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -12,6 +11,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 db = SQL(os.environ.get("DATABASE_URL"))
 helpers.db = SQL(os.environ.get("DATABASE_URL"))
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 # Ensure responses aren't cached
 @app.after_request
@@ -21,18 +21,14 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-# Configure session to use filesystem (instead of signed cookies)
-# app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+app.config["SECRET_KEY"] = '33f82044d3d0d13c6e0bb0cb7ac827c42668aa29c34f2bec'
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
 
-    # Forget any user_id
-    session.clear()
+    if 'url' not in session:
+        session.clear()
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
@@ -57,7 +53,10 @@ def login():
         session["user_id"] = rows[0]["id"]
 
         # Redirect user to home page
-        return redirect("/")
+        if 'url' in session:
+            return redirect(session['url'])
+        else:
+            return redirect("/")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
@@ -78,22 +77,16 @@ def logout():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-
-    session.clear()
+    if 'url' not in session:
+        session.clear()
 
     if request.method == 'POST':
         if request.form.get("username") == '':
             return apology("Missing username.")
-
         if request.form.get("password") == '':
             return apology("Missing password.")
-
-        
-        #if request.form.get("password (again)") == '':
         if request.form.get("confirmation") == '':
             return apology("Please confirm password.")
-
-        # if request.form.get("password") != request.form.get("password (again)"):
         if request.form.get("password") != request.form.get("confirmation"):
             return apology("Passwords do not match.")
 
@@ -103,7 +96,6 @@ def register():
         rows = db.execute("SELECT * FROM app_users WHERE username = :username", username=request.form.get("username"))
         if len(rows) >= 1:
                 return apology("Username already taken.")
-
         result = db.execute("INSERT INTO app_users (username, hash) VALUES(:username, :password)",
                            username = request.form.get("username"), password = hashed_password)
         if not result:
@@ -116,115 +108,108 @@ def register():
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
 
-        # Redirect user to home page
-        return redirect("/")
+        if 'url' in session:
+            return redirect(session['url'])
+        else:
+            return redirect("/")
 
     else:
         return render_template("register.html")
 
 # display available flights
 @app.route('/')
-@login_required
+#@login_required
 def display_flights():
     """ Display available departures """ 
-
-    cities = db.execute(
-        """
-        SELECT DISTINCT city
-        FROM app_airports
-        ORDER BY city
-        """
-    )
-
-    # check if any url params have been passed. if not, just display some defaults and empty table
+    cities = db.execute(""" SELECT DISTINCT city FROM app_airports ORDER BY city """)
+    # if no args have been passed, show default blank results page
     if not request.args:
-        departure_city = "Moscow"
-        arrival_city = "St. Petersburg"
-        departure_date = "2020-12-16"
-        arrival_date = "2020-12-17"
-        flight_type = "one-way-nonstop"
-        flights = []
-        flight_id_string = ""
-        return render_template(
-            'display_flights.html', flights=flights, cities=cities, departure_city=departure_city,
-            arrival_city=arrival_city, flight_type=flight_type, departure_date=departure_date,
-            arrival_date=arrival_date, flight_id_string=flight_id_string
-        )
-
-    # at this point, a get request was submitted by form, passing in the following args
-    departure_city = request.args.get('departure-city')
-    arrival_city = request.args.get('arrival-city')
-    departure_date = request.args.get('departure-date')
-    arrival_date = request.args.get('arrival-date')
-    flight_type = request.args.get('flight-type')
-    flight_id_string = request.args.get('flight-id-string')
-
-    if not request.args.get('chosen-departure-flight-id'):
-        # if get request was a result of simple search... simply display flights and empty cart
-        flight_id_string = "" 
-        if flight_type == "one-way-nonstop" or flight_type == "round-trip":
-            flights = get_nonstop_departures(departure_city, arrival_city, departure_date) 
-        elif flight_type == "one-way-connection":
-            flights = get_first_leg(departure_city, arrival_city, departure_date)
+        session['display-form-parameters'] = {
+            'departure-city': "Moscow",
+            'arrival-city': "St. Petersburg",
+            'departure-date': "2020-12-16",
+            'arrival-date': "2020-12-17",
+            'flight-type': "one-way-nonstop",
+        }
+        session['flights-cart'] = []
+        form_parameters = session['display-form-parameters']
+        return render_template('display_flights.html', flights=[], cities=cities, form_parameters=form_parameters)
+    # else, args came from either search form or the selection of a flight
+    form_parameters = session['display-form-parameters']
+    # if from search form, store args and display relevant flights
+    if request.args.get('flight-type'):
+        session['flights-cart'].clear()
+        form_parameters['departure-city'] = request.args.get('departure-city')
+        form_parameters['arrival-city'] = request.args.get('arrival-city')
+        form_parameters['departure-date'] = request.args.get('departure-date')
+        form_parameters['arrival-date'] = request.args.get('arrival-date')
+        form_parameters['flight-type'] = request.args.get('flight-type')
+        session['flights-cart'] = [] # empty cart
+        if form_parameters['flight-type'] == "one-way-nonstop" or form_parameters['flight-type'] == "round-trip":
+            flights = get_nonstop_departures(
+                form_parameters['departure-city'], form_parameters['arrival-city'], form_parameters['departure-date'])
+        elif form_parameters['flight-type'] == "one-way-connection":
+            flights = get_first_leg(
+                form_parameters['departure-city'], form_parameters['arrival-city'], form_parameters['departure-date'])
+        format_flights(flights)
+        return render_template('display_flights.html', flights=flights, cities=cities, form_parameters=form_parameters)
+    # otherwise, a flight must have been selected from table
     else:
-        # otherwise, a flight must have been selected from table
-        flight_id_string += request.args.get('chosen-departure-flight-id')
-        flight_id_string += ','
-        flight_id_list = flight_id_string.split(',')[:-1]
-
-        if flight_type == "one-way-nonstop" and len(flight_id_list) == 1:
-            # one-way nonstop trips need max one flight, so send to checkout
-            ticket_list = get_order_from_one_id(flight_id_list[0])
+        new_id = request.args.get('chosen-departure-flight-id')
+        session['flights-cart'].append(new_id)
+        # three data processing possibilities, one for each type of flight
+        if form_parameters['flight-type'] == "one-way-nonstop" and len(session['flights-cart']) == 1:
+            # one-way nonstop trips need max one flight, show trip confirmation
+            ticket_list = get_order_from_one_id(session['flights-cart'][0])
+            session['url'] = url_for('checkout')
             format_flights(ticket_list)
-            return render_template('confirm_trip.html', order=ticket_list, flight_id_string = flight_id_string)
-        elif flight_type == "one-way-connection":
-            if len(flight_id_list) == 1:
-                # next, we must find the flight for the second leg of the trip
-                flights = get_second_leg(request.args.get('arrival-airport'), request.args.get('form-arrival-city'), request.args.get('chosen-arrival-date'))
-            elif len(flight_id_list) == 2:
-                # it's now time to render ticket checkout
-                ticket_list = get_order_from_two_id(flight_id_list[0], flight_id_list[1])
+            return render_template('confirm_trip.html', order=ticket_list)
+        elif form_parameters['flight-type'] == "one-way-connection":
+            if len(session['flights-cart']) == 1:
+                # first part of connection has been found, must move on to next leg
+                flights = get_second_leg(
+                    request.args.get('chosen-arrival-airport'), form_parameters['arrival-city'],
+                    request.args.get('chosen-arrival-date'))
+                # display second leg flights
+                return render_template('display_flights.html', flights=flights, cities=cities, form_parameters=form_parameters)
+            elif len(session['flights-cart']) == 2:
+                # second part of connection found, show trip confirmation
+                ticket_list = get_order_from_two_id(session['flights-cart'][0], session['flights-cart'][1])
+                session['url'] = url_for('checkout')
                 format_flights(ticket_list)
-                return render_template('confirm_trip.html', order=ticket_list, flight_id_string = flight_id_string)
-        elif flight_type == "round-trip":
-            if len(flight_id_list) == 1:
-                # we must find the return flight for the arrival leg of the trip
-                flights = get_roundtrip_arrivals(request.args.get('departure-airport'), request.args.get('arrival-airport'),
-                request.args.get('chosen-arrival-date'), arrival_date)
-            elif len(flight_id_list) == 2:
-                # it's now time to render ticket checkout
-                ticket_list = get_order_from_two_id(flight_id_list[0], flight_id_list[1])
+                return render_template('confirm_trip.html', order=ticket_list)
+        elif form_parameters['flight-type'] == "round-trip":
+            if len(session['flights-cart']) == 1:
+                # departure selected, must show arrivals
+                flights = get_roundtrip_arrivals(
+                    request.args.get('chosen-departure-airport'), request.args.get('chosen-arrival-airport'),
+                    request.args.get('chosen-arrival-date'), form_parameters['arrival-date'])
+                return render_template('display_flights.html', flights=flights, cities=cities, form_parameters=form_parameters)
+            elif len(session['flights-cart']) == 2:
+                # arrival selected, show trip confirmation 
+                ticket_list = get_order_from_two_id(session['flights-cart'][0], session['flights-cart'][1])
+                session['url'] = url_for('checkout')
                 format_flights(ticket_list)
-                return render_template('confirm_trip.html', order=ticket_list, flight_id_string = flight_id_string)
-
-    # format data in returned rows
-    format_flights(flights)
-
-    return render_template(
-        'display_flights.html', flights=flights, cities=cities, departure_city=departure_city,
-        arrival_city=arrival_city, flight_type=flight_type, departure_date=departure_date,
-        arrival_date=arrival_date, flight_id_string=flight_id_string
-    )
+                return render_template('confirm_trip.html', order=ticket_list)
 
 @app.route('/checkout', methods=["GET", "POST"])
 @login_required
 def checkout():
     if request.method == "GET":
-        # convert flight id string into list
-        flight_id_str = request.args.get('flight-id-string')
-        flight_id_list = flight_id_str.split(',')[:-1]
-        flights = []
+        flight_id_list = session['flights-cart']
 
+        if not flight_id_list:
+            return redirect(url_for('display_flights'))
         if len(flight_id_list) == 1:
             flights = get_order_from_one_id(flight_id_list[0])
         elif len(flight_id_list) == 2:
             flights = get_order_from_two_id(flight_id_list[0], flight_id_list[1])
-            
+
         for flight in flights:
-            flight['seats_table'] = 'test'
             flight['seats_table'] = get_seats_available(flight['flight_id'])
 
-        return render_template('checkout.html', flights=flights, flight_id_str=flight_id_str)
+        return render_template('checkout.html', flights=flights)
+
     else:
         # get personal info
         name = request.form.get('name')
@@ -233,8 +218,9 @@ def checkout():
         user_id = session["user_id"]
 
         # get seat and fare info
-        first_seat, first_fare, second_seat, second_fare, flight_id_str = "", "", "", "", ""
-        flight_id_list, seat_fare_list, seat_list, price_list = [], [], [], []
+        flight_id_list = session['flights-cart']
+        first_seat, first_fare, second_seat, second_fare = "", "", "", ""
+        seat_fare_list, seat_list, price_list = [], [], []
         if request.form.get('seat-fare-0'):
             seat_fare_list = eval(request.form.get('seat-fare-0'))
             seat_list.append(seat_fare_list[0])
@@ -251,9 +237,6 @@ def checkout():
                 price_list.append(250)
             else:
                 price_list.append(800)
-        if request.form.get('flight-id-string'):
-            flight_id_str = request.form.get('flight-id-string')
-            flight_id_list = flight_id_str.split(',')[:-1]
 
         # get num bags
         num_bags = request.form.get('num-bags')
@@ -262,21 +245,17 @@ def checkout():
         for i in range(len(flight_id_list)):
             add_ticket(user_id, name, email, phone_num, flight_id_list[i], seat_list[i], price_list[i], num_bags)
 
-        # determine total cost
         return redirect('/my_flights')
-
 
 @app.route('/my_flights')
 @login_required
 def my_flights():
     my_tickets = get_personal_flights(session["user_id"])
-    
+
     # add flight details to tickets dict
     for ticket in my_tickets:
-        #ticket['flight_code'] = get_flight_code(ticket['flight_id'])
         ticket['flight_details'] = get_flight_details(ticket['flight_id'])
         format_flight(ticket['flight_details'])
-
     return render_template('my_flights.html', tickets=my_tickets)
 
 if __name__ =="__main__":
